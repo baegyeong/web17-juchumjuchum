d6b7135ad751ad19ff9b40bf8088439ca4eae392
baegyeong <beth0929@naver.com>
2025-02-04 01:03:20 +0900

    🚚 chore: vitest config파일을 vite와 통합

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +9,24 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    setupFiles: ['./vitest.setup.ts'],
+    environment: 'jsdom',
+    globals: true,
+    deps: {
+      optimizer: {
+        web: {
+          include: ['vitest-canvas-mock'],
+        },
+      },
+    },
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    environmentOptions: {
+      jsdom: {
+        resources: 'usable',
+      },
     },
   },
 });
